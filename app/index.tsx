ad916bbd0ad61753e7c2f33a315a152c18b99b29
huuthanhdev <thanhhuuhoang.sd@gmail.com>
2025-08-01 08:36:39 +0700

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import { Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const App = () => {
+    return (
+        <SafeAreaView style={{flex:1}}>
+            <Text>ToDo App Hello</Text>
+        </SafeAreaView>
+    );
+};
+
+export default App;
